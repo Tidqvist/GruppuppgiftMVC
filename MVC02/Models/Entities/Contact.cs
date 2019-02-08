e399d7b41a1cbc0c2bd18729ba8aef0801a177e8
Bebolicious <a.zetterberg@hotmail.com>
2019-02-08 11:39:31 +0100

    added contact page

---
+++ MVC02/Models/Entities/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC02.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [Required][MaxLength(length: 100, ErrorMessage="Message is too long. Maximum characters is 100")]
+        public string Message { get; set; }
+
+    }
+}
